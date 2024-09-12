7e265bf7d6bea2fa52b8ec85e176e11ba1ce5832
umesh <umesh37kr@gmail.com>
2024-09-12 10:59:08 +0530

    add shadcn ui theme

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Welcome to my school</h1>
-      <Button variant="outline">Button</Button>
+      <Button>Button</Button>
     </main>
   );
 }
