759f17f600d33a75393faa95f399bd6d6c640385
umesh <umesh37kr@gmail.com>
2024-09-13 12:07:18 +0530

    add nav link and logo

--- src/components/custom/header.jsx
+++ src/components/custom/header.jsx
@@ -1,8 +1,36 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 const Header = () => {
   return (
-    <header className="bg-slate-500">
-      <nav className="container py-5">
-        <div>header</div>
+    <header className="bg-white">
+      <nav className="container flex items-center justify-between py-2">
+        <div>
+          <Link href="/">
+            <Image src={"/logo.png"} alt="logo" height={100} width={100} />
+          </Link>
+        </div>
+        <div>
+          <ul className="flex items-center justify-between space-x-5">
+            <li>
+              <Link href="/login">About us</Link>
+            </li>
+            <li>
+              <Link href="/login">Admission</Link>
+            </li>
+            <li>
+              <Link href="/login">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/login">Contact</Link>
+            </li>
+            <li>
+              <Button asChild className="mr-4">
+                <Link href="/login">Login</Link>
+              </Button>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
