00494c3456b198b13c0b986220977c0c9a677fb2
umesh <umesh37kr@gmail.com>
2024-09-17 12:43:41 +0530

    update color variable

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -8,11 +8,11 @@ export default function Home() {
       <section className="" style={{ background: "#fff8e8" }}>
         <div className="container flex items-center justify-center">
           <div className=" ">
-            <h1 className="text-6xl text-gray-700">
+            <h1 className="text-6xl text-heading">
               We Prepare Your <br />
               <span className="text-primary">Child</span> For Life
             </h1>
-            <p className="text-lg text-gray-600 py-6 leading-snug">
+            <p className="text-lg text-paragraph py-6 max-w-lg leading-snug">
               A Place Where Young Hearts Grow, Minds Explore, and Futures Take
               Shape!
             </p>
@@ -23,6 +23,18 @@ export default function Home() {
           <div className="flex items-center justify-center">
             <Image src={"/hero1.jpg"} alt="hero" width={600} height={600} />
           </div>
+        </div>
+      </section>
+      {/* About school */}
+      <section>
+        <div className="container py-5">
+          <div>
+            <h3 className="italic text-primary font-semibold">Welcome to</h3>
+            <h1 className="text-xl font-semibold text-heading">
+              First School International, Garhwa
+            </h1>
+          </div>
+          <div>right</div>
         </div>
       </section>
     </>
