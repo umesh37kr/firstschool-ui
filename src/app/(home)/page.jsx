a2ed11d48edb50fa0ac82351b483838e8683d0cb
umesh <umesh37kr@gmail.com>
2024-09-18 10:49:21 +0530

    add about school

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <section className="" style={{ background: "#fff8e8" }}>
         <div className="container flex items-center justify-center">
-          <div className=" ">
+          <div>
             <h1 className="text-6xl text-heading">
               We Prepare Your <br />
               <span className="text-primary">Child</span> For Life
@@ -27,14 +27,40 @@ export default function Home() {
       </section>
       {/* About school */}
       <section>
-        <div className="container py-5">
+        <div className="container py-5 flex items-center justify-center">
           <div>
             <h3 className="italic text-primary font-semibold">Welcome to</h3>
             <h1 className="text-xl font-semibold text-heading">
               First School International, Garhwa
             </h1>
+            <p className="leading-relaxed text-paragraph text-justify mt-4">
+              At{" "}
+              <span className="text-primary">
+                First School International, Garhwa
+              </span>
+              , we are dedicated to creating a nurturing and stimulating
+              environment where children can thrive both academically and
+              personally. Our school focuses on fostering creativity, curiosity,
+              and a love for learning from an early age. With a strong emphasis
+              on holistic development, we provide a balanced curriculum that
+              integrates core subjects with arts, sports, and hands-on
+              activities. Our experienced and passionate educators work closely
+              with each child, helping them build confidence, critical thinking
+              skills, and strong social connections. At First School
+              International, we believe in unlocking every child's potential,
+              ensuring they are well-prepared for future academic success and
+              personal growth.
+            </p>
           </div>
-          <div>right</div>
+          <div className="flex items-center justify-center pl-5 mt-20">
+            <Image
+              src={"/school-building.jpg"}
+              alt="school-building"
+              height={5000}
+              width={5000}
+              className="rounded-xl"
+            />
+          </div>
         </div>
       </section>
     </>
