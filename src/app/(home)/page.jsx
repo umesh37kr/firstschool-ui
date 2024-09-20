1188692964f5be23e6824513419a7e5c84bf50b2
umesh <umesh37kr@gmail.com>
2024-09-20 11:08:40 +0530

    change banner icons and add waver

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { MoveRight, School } from "lucide-react";
+import {
+  Baby,
+  BabyIcon,
+  BookOpenCheck,
+  MoveRight,
+  School,
+  Users,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -64,43 +71,76 @@ export default function Home() {
         </div>
       </section>
       {/* number banners */}
-      <section className="bg-cyan-700 my-6">
+      <section className="bg-cyan-700">
         {/* <div className="container "> */}
         <div className="grid grid-cols-4 gap-5">
           <div className="py-10">
-            <School className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
-            <h1 className="text-4xl font-bold text-center text-gray-200">
+            <div className="flex justify-center">
+              <BookOpenCheck className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
+            </div>
+            <h1 className="text-4xl font-bold text-center text-gray-200 py-2">
               10+
             </h1>
             <p className="text-white text-center">Years of Experience</p>
           </div>
-          <div className="py-10 bg-red-500">
+          <div className="py-10">
             <span className="items-center"></span>
-            <p className="text-center">
-              <School color="white" size={48} />
-            </p>
-            <h1 className="text-4xl font-bold text-center text-gray-200">
-              10+
+            <div className="flex justify-center">
+              <Users className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
+            </div>
+            <h1 className="text-4xl font-bold text-center text-gray-200 py-2">
+              15+
             </h1>
-            <p className="text-white text-center">Years of Experience</p>
+            <p className="text-white text-center">Experts Instructors</p>
+          </div>
+          <div className="py-10 ">
+            <div className="flex justify-center">
+              <Baby className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
+            </div>
+            <h1 className="text-4xl font-bold text-center text-gray-200 py-2">
+              1200+
+            </h1>
+            <p className="text-white text-center">Students Enrolled</p>
           </div>
           <div className="py-10">
-            <School className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
-            <h1 className="text-4xl font-bold text-center text-gray-200">
-              10+
+            <div className="flex justify-center">
+              <School className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
+            </div>
+            <h1 className="text-4xl font-bold text-center text-gray-200 py-2">
+              25+
             </h1>
-            <p className="text-white text-center">Years of Experience</p>
-          </div>
-          <div className="py-10">
-            <School className="text-white h-11 w-11  stroke-white stroke-1 stroke-center" />
-            <h1 className="text-4xl font-bold text-center text-gray-200">
-              10+
-            </h1>
-            <p className="text-white text-center">Years of Experience</p>
+            <p className="text-white text-center">Number of Classes</p>
           </div>
         </div>
+        {/* <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            className=""
+          >
+            <path
+              fill="#0099ff"
+              fill-opacity="1"
+              d="M0,64L48,69.3C96,75,192,85,288,80C384,75,480,53,576,42.7C672,32,768,32,864,42.7C960,53,1056,75,1152,69.3C1248,64,1344,32,1392,16L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+          </svg>
+        </div> */}
         {/* </div> */}
       </section>
+      <div className="-mt-8">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          className="w-full"
+        >
+          <path
+            fill="#0e7490"
+            // fill="#0e7400"
+            fill-opacity="1"
+            d="M0,64L48,69.3C96,75,192,85,288,80C384,75,480,53,576,42.7C672,32,768,32,864,42.7C960,53,1056,75,1152,69.3C1248,64,1344,32,1392,16L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
     </>
   );
 }
