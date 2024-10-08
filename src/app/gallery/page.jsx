0b18409ec4c3945b2cb998f34c032b3fb2b506d1
umesh <umesh37kr@gmail.com>
2024-10-08 12:37:00 +0530

    display image from json data

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,70 +1,21 @@
 import Image from "next/image";
 import React from "react";
-
-const gallery = [
-  {
-    id: 1,
-    name: "images.jpg",
-    events: "no bag day",
-  },
-  {
-    id: 2,
-    name: "images(1).jpg",
-    events: "no bag day",
-  },
-  {
-    id: 16,
-    name: "images(15).jpg",
-    events: "no bag day",
-  },
-  {
-    id: 17,
-    name: "images(17).jpg",
-    events: "no bag day",
-  },
-  {
-    id: 18,
-    name: "images(1).jpg",
-    events: "no bag day",
-  },
-  {
-    id: 19,
-    name: "images(10).jpg",
-    events: "no bag day",
-  },
-  {
-    id: 20,
-    name: "images.jpg",
-    events: "no bag day",
-  },
-  {
-    id: 21,
-    name: "images18.jpg",
-    events: "no bag day",
-  },
-];
+import { gallery } from "@/app/db/gallery.json";
+import Link from "next/link";
 
 const Gallery = () => {
   return (
     <>
-      <div>
-        <Image
-          src={"/gellery/images.jpg"}
-          width={300}
-          height={250}
-          alt={`images.name`}
-        />
-        <Image src={"/hero1.jpg"} width={300} height={250} alt={`hero`} />;
-        {/* {gallery.map((images) => {
-            <a href="">
+      <div className="">
+        {gallery.map((img, index) => (
           <Image
-            src={`/gellery/images.jpg`}
+            key={`${img.id}`}
+            src={`/gallery/${img.name}`}
             width={300}
             height={250}
-            alt={`images.name`}
-          />;
-             </a>;
-        })} */}
+            alt={`${img.name}`}
+          />
+        ))}
       </div>
     </>
   );
