85cb9aaeeea706f36144e5c981967740f7146a41
umesh <umesh37kr@gmail.com>
2024-10-10 11:19:43 +0530

    add image hover

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -6,15 +6,21 @@ import Link from "next/link";
 const Gallery = () => {
   return (
     <>
-      <div className="">
+      <div className="flex flex-wrap items-center justify-center mt-10">
         {gallery.map((img, index) => (
-          <Image
+          <a
+            href=""
             key={`${img.id}`}
-            src={`/gallery/${img.name}`}
-            width={300}
-            height={250}
-            alt={`${img.name}`}
-          />
+            className="w-1/5 cursor-pointer hover:scale-110 transition-all ease-linear"
+          >
+            <Image
+              src={`/gallery/${img.name}`}
+              width={300}
+              height={250}
+              alt={`${img.name}`}
+              // className="w-full h-64 object-cover"
+            />
+          </a>
         ))}
       </div>
     </>
