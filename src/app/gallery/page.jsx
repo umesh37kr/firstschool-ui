5b71f35b915d97713d4d61fbb3d5e4475cb02de2
umesh <umesh37kr@gmail.com>
2024-10-14 11:45:44 +0530

    add gallery button

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,16 +1,26 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { gallery } from "@/app/db/gallery.json";
 import Link from "next/link";
+import {
+  ArrowLeft,
+  ArrowRight,
+  ChevronLeft,
+  ChevronRight,
+  CircleX,
+} from "lucide-react";
 
 const Gallery = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  console.log("galary::", gallery);
   return (
     <>
       <div className="flex flex-wrap items-center justify-center mt-10">
         {gallery.map((img, index) => (
           <a
             href=""
-            key={`${img.id}`}
+            key={`${index}`}
             className="w-1/5 cursor-pointer hover:scale-110 transition-all ease-linear"
           >
             <Image
@@ -22,6 +32,31 @@ const Gallery = () => {
             />
           </a>
         ))}
+      </div>
+      <div className="fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-85 z-50 flex justify-center items-center gap-3">
+        <a href="" className="absolute top-4 right-4 cursor-pointer">
+          <CircleX width={40} height={40} color="red" />
+        </a>
+        <a
+          href=""
+          className="absolute left-6 bg-green-500 rounded-full hover:translate-x-2 transition-all ease-linear"
+        >
+          <ChevronLeft width={40} height={40} color="white" />
+        </a>
+        <div>
+          <Image
+            src={`/gallery/${gallery[selectedImg].name}`}
+            width={650}
+            height={500}
+            alt={`/gallery/${gallery[selectedImg].name}`}
+          />
+        </div>
+        <a
+          href=""
+          className="absolute right-6 bg-green-500 rounded-full hover:-translate-x-2 transition-all ease-linear"
+        >
+          <ChevronRight width={40} height={40} color="white" />
+        </a>
       </div>
     </>
   );
