6cdbcfe3eacda061fca2f66972707a50b9fb2f83
umesh <umesh37kr@gmail.com>
2024-10-16 11:51:22 +0530

    image galley popup functioality3

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -6,13 +6,26 @@ import { ChevronLeft, ChevronRight, CircleX } from "lucide-react";
 
 const Gallery = () => {
   const [selectedImg, setSelectedImg] = useState(0);
-  console.log("galary::", gallery);
+  const [imgPop, setImgPop] = useState(false);
+  const swipeImg = (moveType) => {
+    if (moveType == "prev") {
+      if (selectedImg == 0) setSelectedImg(gallery.length - 1);
+      else setSelectedImg(selectedImg - 1);
+    }
+    if (moveType == "next") {
+      if (selectedImg == gallery.length - 1) setSelectedImg(0);
+      else setSelectedImg(selectedImg + 1);
+    }
+  };
   return (
     <>
       <div className="flex flex-wrap items-center justify-center mt-10">
         {gallery.map((img, index) => (
           <a
-            href=""
+            onClick={() => {
+              setSelectedImg(index);
+              setImgPop(true);
+            }}
             key={`${index}`}
             className="w-1/5 cursor-pointer hover:scale-110 transition-all ease-linear"
           >
@@ -26,31 +39,57 @@ const Gallery = () => {
           </a>
         ))}
       </div>
-      <div className="fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-85 z-50 flex justify-center items-center gap-3">
-        <a href="" className="absolute top-4 right-4 cursor-pointer">
-          <CircleX width={40} height={40} color="red" />
-        </a>
-        <a
-          href=""
-          className="absolute left-6 bg-green-500 rounded-full hover:translate-x-2 transition-all ease-linear"
-        >
-          <ChevronLeft width={40} height={40} color="white" />
-        </a>
-        <div>
-          <Image
-            src={`/gallery/${gallery[selectedImg].name}`}
-            width={650}
-            height={500}
-            alt={`/gallery/${gallery[selectedImg].name}`}
-          />
+      {imgPop && (
+        <div className="fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-85 z-50 flex justify-center items-center gap-3">
+          <a
+            onClick={() => setImgPop(false)}
+            className="absolute top-4 right-4 cursor-pointer"
+          >
+            <CircleX width={40} height={40} color="red" />
+          </a>
+          <a
+            onClick={() => swipeImg("prev")}
+            className="absolute left-6 bg-green-500 rounded-full hover:translate-x-2 transition-all ease-linear"
+          >
+            <ChevronLeft width={40} height={40} color="white" />
+          </a>
+          <div>
+            <Image
+              src={`/gallery/${gallery[selectedImg].name}`}
+              width={650}
+              height={500}
+              alt={`/gallery/${gallery[selectedImg].name}`}
+            />
+          </div>
+          <a
+            onClick={() => swipeImg("next")}
+            className="absolute right-6 bg-green-500 rounded-full hover:-translate-x-2 transition-all ease-linear"
+          >
+            <ChevronRight width={40} height={40} color="white" />
+          </a>
+          <nav className="absolute left-0 bottom-0 right-0 flex justify-center gap-3">
+            {gallery.map((img, index) => (
+              <a
+                onClick={() => {
+                  setSelectedImg(index);
+                  setImgPop(true);
+                }}
+                key={`${index}`}
+                className={`hover:-translate-y-2 cursor-pointer transition-all ease-linear ${
+                  selectedImg == index ? "-translate-y-2" : ""
+                } `}
+              >
+                <Image
+                  src={`/gallery/${img.name}`}
+                  width={500}
+                  height={650}
+                  alt={`${img.name}`}
+                />
+              </a>
+            ))}
+          </nav>
         </div>
-        <a
-          href=""
-          className="absolute right-6 bg-green-500 rounded-full hover:-translate-x-2 transition-all ease-linear"
-        >
-          <ChevronRight width={40} height={40} color="white" />
-        </a>
-      </div>
+      )}
     </>
   );
 };
