a0e89cbf4a431e3138cfa15cff40deedd548781c
umesh <umesh37kr@gmail.com>
2024-10-17 12:06:41 +0530

    add login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="w-screen h-screen bg-gray-100 flex justify-center items-center">
+      <div className="h-96 w-1/3 bg-white flex justify-center">
+        <h1 className="text-heading text-2xl font-semibold py-2">Log In</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
