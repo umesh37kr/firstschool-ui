ac282e281540697cd618a09fcd7df7a34c547f6a
umesh <umesh37kr@gmail.com>
2024-12-24 18:07:41 +0530

    add link for admin dashboard

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,59 +1,21 @@
-"use client";
 import React from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import Link from "next/link";
 
 const Login = () => {
-  // 1. Define your form.
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  });
-  const onSubmit = () => {
-    alert("form submitted..");
-  };
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="flex justify-center items-center h-56">
+      <h1>
+        Please{" "}
+        <Link
+          target="_blank"
+          href={"http://localhost:5173/login"}
+          className="text-blue-700 underline"
+        >
+          click here
+        </Link>{" "}
+        to login admin dashboard
+      </h1>
+    </div>
   );
 };
 export default Login;
